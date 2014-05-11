100fe7d02362335efd645eb7d79b093df3bdce89
Jeff <jeffrey.ragazzini@gmail.com>
2014-05-11 19:47:08 -0400

    Create plot4.r

---
+++ plot4.r
@@ -0,0 +1,41 @@
+fileName <- "household_power_consumption.txt"
+data <- read.table(fileName, sep=";", nrows=2880, skip=66637, col.names=
+                     c("Date", "Time", "GAP", "GRP", "V", "GI", "Sub_Metering_1", 
+                       "Sub_Metering_2", "Sub_Metering_3"))
+
+data$Date <- as.character(data$Date)
+data$Time <- as.character(data$Time)
+
+newTime <- paste(data$Date, data$Time, sep=" ")
+newCol <- strptime(newTime, format="%d/%m/%Y %H:%M:%S")
+newPos <- as.POSIXlt(newCol)
+
+xrange <- c(head(newPos,1), tail(newPos,1))
+xrange <- as.numeric(xrange)
+yrange <- c(0, max(data$Sub_Metering_1))
+
+png(filename="plot4.png", width=480, height=480)
+
+par(mfrow=c(2,2))
+
+plot(newPos, data$GAP, main="", 
+     xlab="", ylab="Global Active Power", type="l")
+
+plot(newPos, data$V, main="", xlab="datetime", ylab="Voltage", type="l")
+
+
+plot(newPos, data$Sub_Metering_1, main="", xlim=xrange, ylim=yrange,
+                xlab="", ylab="Energy sub metering", type="l")
+par(new=T)
+plot(newPos, data$Sub_Metering_2, main="", xlim=xrange, ylim=yrange,
+                xlab="", ylab="Energy sub metering", type="l", col="red")
+par(new=T)
+plot(newPos, data$Sub_Metering_3, main="", xlim=xrange, ylim=yrange,
+                xlab="", ylab="Energy sub metering", type="l", col="blue")
+par(new=T)
+legend("topright", col=c("black", "blue", "red"), cex=1, lty=1,
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
+par(new=F)
+
+plot(newPos, data$GRP, main="", xlab="datetime", ylab="Global_reactive_power", type="l")
+dev.off()
